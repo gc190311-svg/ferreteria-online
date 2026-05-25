347d4917bb0e03cf64a6e09c5c48e8a9c8f91ffc
gc190311-svg <gc190311@gmail.com>
2026-05-25 18:44:41 -0500

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "TU_APIKEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECTID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER",
-  appId: "TU_APPID"
+  apiKey: "AIzaSyAEaKesEc9OdvzSiTHW-4DFUmlgSuB7NeY",
+  authDomain: "tienda-online-9d00d.firebaseapp.com",
+  projectId: "tienda-online-9d00d",
+  storageBucket: "tienda-online-9d00d.firebasestorage.app",
+  messagingSenderId: "718461068662",
+  appId: "1:718461068662:web:b7395ca6a46ed24ae28657",
+  measurementId: "G-FM3ZC6ZFK2"
 };
 
 const app = initializeApp(firebaseConfig);
