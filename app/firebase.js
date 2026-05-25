7672760e32d3265451cec23b61b2a6731087ed85
gc190311-svg <gc190311@gmail.com>
2026-05-25 02:22:56 -0500

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_APIKEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECTID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER",
-  appId: "TU_APPID"
+  apiKey: "AIzaSyAEaKesEc9OdvzSiTHW-4DFUmlgSuB7NeY",
+  authDomain: "tienda-online-9d00d.firebaseapp.com",
+  projectId: "tienda-online-9d00d",
+  storageBucket: "tienda-online-9d00d.firebasestorage.app",
+  messagingSenderId: "718461068662",
+  appId: "1:718461068662:web:b7395ca6a46ed24ae28657",
+  measurementId: "G-FM3ZC6ZFK2"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
