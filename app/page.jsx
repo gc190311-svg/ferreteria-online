0828c4bcd70ed638a4d6cf5295d961b134d0cd5b
gc190311-svg <gc190311@gmail.com>
2026-06-20 14:43:07 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -211,8 +211,6 @@ const productosPorCategoria =
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
 
-         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-
   {productosPorCategoria.map((producto) => (
 
     <a
