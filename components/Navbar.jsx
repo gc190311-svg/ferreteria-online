7c3fc0cdb92603c3d736239b223f5aa8c158682d
gc190311-svg <gc190311@gmail.com>
2026-06-21 17:08:27 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,43 +8,73 @@ export default function Navbar() {
 
         {/* BOTÓN CATEGORÍAS */}
 
-        <button className="bg-yellow-500 hover:bg-yellow-400 text-black font-bold text-xl px-10 py-6 transition">
-
+        <button
+          className="
+          bg-yellow-500
+          hover:bg-yellow-400
+          text-black
+          font-bold
+          text-lg
+          px-8
+          py-5
+          transition
+          "
+        >
           ☰ CATEGORÍAS
-
         </button>
 
         {/* MENÚ */}
 
-        <div className="flex-1 flex justify-center gap-20 text-white font-bold text-xl">
+        <div className="flex-1 flex justify-center">
 
-          <a
-            href="#"
-            className="py-6 border-b-4 border-yellow-500 text-yellow-500"
-          >
-            INICIO
-          </a>
+          <div className="flex gap-16 text-white font-bold text-lg">
 
-          <a
-            href="#productos"
-            className="py-6 hover:text-yellow-500 transition"
-          >
-            PRODUCTOS
-          </a>
+            <a
+              href="#"
+              className="
+              py-5
+              border-b-4
+              border-yellow-500
+              text-yellow-500
+              "
+            >
+              INICIO
+            </a>
 
-          <a
-            href="#"
-            className="py-6 hover:text-yellow-500 transition"
-          >
-            OFERTAS
-          </a>
+            <a
+              href="#productos"
+              className="
+              py-5
+              hover:text-yellow-500
+              transition
+              "
+            >
+              PRODUCTOS
+            </a>
 
-          <a
-            href="#contacto"
-            className="py-6 hover:text-yellow-500 transition"
-          >
-            CONTACTO
-          </a>
+            <a
+              href="#"
+              className="
+              py-5
+              hover:text-yellow-500
+              transition
+              "
+            >
+              OFERTAS
+            </a>
+
+            <a
+              href="#contacto"
+              className="
+              py-5
+              hover:text-yellow-500
+              transition
+              "
+            >
+              CONTACTO
+            </a>
+
+          </div>
 
         </div>
 
