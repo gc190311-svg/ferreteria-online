3df922844a50064d8102635f3b6e067b918ef29b
gc190311-svg <gc190311@gmail.com>
2026-06-21 17:37:02 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 export default function Navbar() {
   return (
-    <div className="bg-red-600 text-white text-5xl p-10 text-center">
+    <div className="bg-red-600 text-white text-5xl text-center p-8">
       ESTE ES MI NAVBAR NUEVO
     </div>
   );
