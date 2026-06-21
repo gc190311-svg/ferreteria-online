79cceeec67e3c89f91a884d0efc658a8925d392f
gc190311-svg <gc190311@gmail.com>
2026-06-21 00:46:47 -0500

    Update TopBar.jsx

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,1 +1,37 @@
+export default function TopBar() {
 
+  return (
+
+    <div className="bg-black text-white">
+
+      <div className="max-w-7xl mx-auto px-6 py-2">
+
+        <div className="flex flex-col md:flex-row justify-between items-center gap-2 text-sm">
+
+          <div>
+
+            📞 Atención al cliente: 921 883 870
+
+          </div>
+
+          <div>
+
+            🚚 Delivery a todo Lima
+
+          </div>
+
+          <div>
+
+            ⭐ Calidad garantizada
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+
+}
