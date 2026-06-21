c68c4e2f98f638a3c46f21ba1be8e2bd5dd48732
gc190311-svg <gc190311@gmail.com>
2026-06-21 18:34:58 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,16 +1,16 @@
 export default function Hero() {
   return (
-    <section className="bg-gradient-to-r from-black via-gray-900 to-blue-950 text-white">
+    <section className="bg-blue-900 text-white py-24">
 
-      <div className="max-w-7xl mx-auto px-8 py-24">
+      <div className="max-w-7xl mx-auto px-8">
 
-        <div className="grid lg:grid-cols-2 gap-16 items-center">
+        <div className="grid md:grid-cols-2 gap-16 items-center">
 
           {/* TEXTO */}
 
           <div>
 
-            <h1 className="text-6xl lg:text-7xl font-extrabold leading-tight">
+            <h1 className="text-7xl font-extrabold leading-tight">
 
               Todo lo que
               <br />
@@ -30,39 +30,21 @@ export default function Hero() {
 
             </p>
 
-            <button
-              className="
-              mt-10
-              bg-yellow-500
-              hover:bg-yellow-400
-              text-black
-              font-bold
-              text-xl
-              px-10
-              py-5
-              rounded-2xl
-              transition
-              "
-            >
+            <button className="mt-10 bg-yellow-500 hover:bg-yellow-400 text-black font-bold px-10 py-5 rounded-2xl text-xl">
+
               Ver Productos
+
             </button>
 
           </div>
 
           {/* IMAGEN */}
 
-          <div className="flex justify-center">
+          <div>
 
             <img
               src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1200"
-              alt="Construcción"
-              className="
-              w-[600px]
-              h-[400px]
-              object-cover
-              rounded-3xl
-              shadow-2xl
-              "
+              className="w-full rounded-3xl shadow-2xl"
             />
 
           </div>
