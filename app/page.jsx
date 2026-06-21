e17fdf5e03b4885366725ed2cfe6eaabee9ab586
gc190311-svg <gc190311@gmail.com>
2026-06-21 18:36:38 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,7 @@ import TopBar from "../components/TopBar";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Servicios from "../components/Servicios";
-import Productos from "../components/Productos";
-import Footer from "../components/Footer";
-import WhatsAppButton from "../components/WhatsAppButton";
+
 
 export default function Home() {
 
