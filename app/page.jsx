03bca616d2fc8af17644d27a5077844f2217b276
gc190311-svg <gc190311@gmail.com>
2026-06-21 18:45:02 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,21 +5,13 @@ import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 
-
 export default function Home() {
-
   return (
-
-    <div className="min-h-screen bg-gray-100">
-
+    <>
       <TopBar />
       <Header />
       <Navbar />
       <Hero />
-    
-
-    </div>
-
+    </>
   );
-
 }
