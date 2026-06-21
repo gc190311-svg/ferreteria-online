0511a22f6a89cad1143891a6469093580dc59c7f
gc190311-svg <gc190311@gmail.com>
2026-06-21 18:03:40 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,79 +1,17 @@
 export default function Hero() {
   return (
-    <section className="bg-gradient-to-r from-black via-gray-900 to-blue-950 text-white">
-
-      <div className="max-w-7xl mx-auto px-8 py-20">
-
-        <div className="grid lg:grid-cols-2 gap-16 items-center">
-
-          {/* TEXTO */}
-
-          <div>
-
-            <h1 className="text-5xl lg:text-7xl font-extrabold leading-tight">
-
-              Todo lo que
-              <br />
-              necesitas
-              <br />
-              para tu obra
-
-            </h1>
-
-            <p className="mt-8 text-2xl text-gray-300 leading-relaxed">
-
-              Herramientas, electricidad,
-              construcción, pintura,
-              gasfitería y mucho más.
-
-            </p>
-
-            <div className="mt-10">
-
-              <a
-                href="#productos"
-                className="
-                inline-block
-                bg-yellow-500
-                hover:bg-yellow-400
-                text-black
-                font-bold
-                text-xl
-                px-10
-                py-5
-                rounded-2xl
-                transition
-                "
-              >
-                Ver Productos
-              </a>
-
-            </div>
-
-          </div>
-
-          {/* IMAGEN */}
-
-          <div>
-
-            <img
-              src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1200"
-              alt="Construcción"
-              className="
-              w-full
-              rounded-3xl
-              shadow-2xl
-              object-cover
-              "
-            />
-
-          </div>
-
-        </div>
-
-      </div>
-
-    </section>
+    <div
+      style={{
+        background: "blue",
+        color: "white",
+        padding: "100px",
+        fontSize: "60px",
+        textAlign: "center"
+      }}
+    >
+      ESTE ES MI NUEVO HERO
+    </div>
   );
 }
-      
+          
+        
