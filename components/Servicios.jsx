78077d87007658553f89d78e6b177d8ce4bfd670
gc190311-svg <gc190311@gmail.com>
2026-06-21 18:18:58 -0500

    Update Servicios.jsx

--- components/Servicios.jsx
+++ components/Servicios.jsx
@@ -1,111 +1,17 @@
 export default function Servicios() {
-
   return (
+    <section className="bg-green-600 h-[300px] flex items-center justify-center">
 
-    <section className="max-w-7xl mx-auto px-6 py-20">
+      <h1 className="text-6xl text-white font-bold">
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        SERVICIOS NUEVOS
 
-        {/* DELIVERY */}
-
-        <div className="bg-white rounded-3xl shadow-lg p-8 text-center hover:shadow-2xl transition">
-
-          <div className="text-5xl">
-
-            🚚
-
-          </div>
-
-          <h3 className="font-bold text-2xl mt-5">
-
-            Delivery rápido
-
-          </h3>
-
-          <p className="text-gray-500 mt-4">
-
-            Envíos a todo Lima Metropolitana.
-
-          </p>
-
-        </div>
-
-        {/* CALIDAD */}
-
-        <div className="bg-white rounded-3xl shadow-lg p-8 text-center hover:shadow-2xl transition">
-
-          <div className="text-5xl">
-
-            ⭐
-
-          </div>
-
-          <h3 className="font-bold text-2xl mt-5">
-
-            Calidad garantizada
-
-          </h3>
-
-          <p className="text-gray-500 mt-4">
-
-            Productos de las mejores marcas.
-
-          </p>
-
-        </div>
-
-        {/* PRECIOS */}
-
-        <div className="bg-white rounded-3xl shadow-lg p-8 text-center hover:shadow-2xl transition">
-
-          <div className="text-5xl">
-
-            💰
-
-          </div>
-
-          <h3 className="font-bold text-2xl mt-5">
-
-            Mejores precios
-
-          </h3>
-
-          <p className="text-gray-500 mt-4">
-
-            Ofertas especiales todos los días.
-
-          </p>
-
-        </div>
-
-        {/* ASESORÍA */}
-
-        <div className="bg-white rounded-3xl shadow-lg p-8 text-center hover:shadow-2xl transition">
-
-          <div className="text-5xl">
-
-            👨‍🔧
-
-          </div>
-
-          <h3 className="font-bold text-2xl mt-5">
-
-            Atención personalizada
-
-          </h3>
-
-          <p className="text-gray-500 mt-4">
-
-            Te asesoramos en cada compra.
-
-          </p>
-
-        </div>
-
-      </div>
+      </h1>
 
     </section>
-
   );
-
 }
+
+     
+
+     
