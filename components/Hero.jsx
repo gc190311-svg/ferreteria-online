055d8193f7018afae186c3161dce90d52e84d598
gc190311-svg <gc190311@gmail.com>
2026-06-21 19:01:27 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -53,7 +53,7 @@ export default function Hero() {
           <div>
 
             <img
-              src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1200"
+              src=
               alt="Construcción"
               className="
                 w-full
