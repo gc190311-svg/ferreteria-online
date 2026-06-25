bdc2bb16411c7eda5660e8a1169569eb5ddc0838
gc190311-svg <gc190311@gmail.com>
2026-06-24 20:26:09 -0500

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,1 +1,52 @@
+"use client";
 
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+
+  const router = useRouter();
+
+  const loginGoogle = async () => {
+
+    try {
+
+      const provider = new GoogleAuthProvider();
+
+      const result =
+        await signInWithPopup(auth, provider);
+
+      console.log(result.user.email);
+
+      router.push("/admin");
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  };
+
+  return (
+
+    <div className="min-h-screen flex items-center justify-center">
+
+      <button
+        onClick={loginGoogle}
+        className="
+          bg-yellow-500
+          px-8
+          py-4
+          rounded-xl
+          font-bold
+        "
+      >
+        Ingresar con Google
+      </button>
+
+    </div>
+
+  );
+}
