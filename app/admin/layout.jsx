bce47381cb7c1b94c64bfdeadc2981b3065ab823
gc190311-svg <gc190311@gmail.com>
2026-06-25 16:25:56 -0500

    Create layout.jsx

---
+++ app/admin/layout.jsx
@@ -0,0 +1,22 @@
+import Sidebar from "../../components/admin/Sidebar";
+import Topbar from "../../components/admin/Topbar";
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+
+      <Sidebar />
+
+      <div className="flex-1">
+
+        <Topbar />
+
+        <main className="p-8">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
+  );
+}
