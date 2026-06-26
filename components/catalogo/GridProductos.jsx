f83e7c35ff3c3c0f16a68554da147f1ed7f6c388
gc190311-svg <gc190311@gmail.com>
2026-06-25 23:48:24 -0500

    Update GridProductos.jsx

--- components/catalogo/GridProductos.jsx
+++ components/catalogo/GridProductos.jsx
@@ -1,1 +1,70 @@
+"use client";
 
+import { useEffect, useState } from "react";
+
+import {
+    collection,
+    getDocs
+} from "firebase/firestore";
+
+import { db } from "../../app/firebase";
+
+import CardProducto from "./CardProducto";
+
+export default function GridProductos() {
+
+    const [productos,setProductos]=useState([]);
+
+    useEffect(()=>{
+
+        cargarProductos();
+
+    },[]);
+
+    async function cargarProductos(){
+
+        const querySnapshot=await getDocs(
+            collection(db,"productos")
+        );
+
+        const lista=[];
+
+        querySnapshot.forEach((doc)=>{
+
+            lista.push({
+                id:doc.id,
+                ...doc.data()
+            });
+
+        });
+
+        setProductos(lista);
+
+    }
+
+    return(
+
+        <div
+            className="
+            grid
+            md:grid-cols-2
+            lg:grid-cols-3
+            xl:grid-cols-4
+            gap-8
+            "
+        >
+
+            {productos.map((producto)=>(
+
+                <CardProducto
+                    key={producto.id}
+                    producto={producto}
+                />
+
+            ))}
+
+        </div>
+
+    );
+
+}
