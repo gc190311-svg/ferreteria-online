f5c707fd4eddb9321638773521c3658da376a6f5
gc190311-svg <gc190311@gmail.com>
2026-06-26 00:43:32 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,59 +3,30 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Navbar({
-  setCategoriaSeleccionada,
-}) {
+export default function Navbar({ setCategoriaSeleccionada }) {
 
   const pathname = usePathname();
-  
+
   return (
     <nav className="bg-black border-t border-gray-800 border-b border-gray-800">
 
       <div className="max-w-7xl mx-auto flex items-center">
 
         {/* CATEGORÍAS */}
-
         <div className="relative group w-[250px]">
 
-          <button
-            className="
-              w-full
-              bg-yellow-500
-              hover:bg-yellow-400
-              text-black
-              font-bold
-              text-lg
-              py-5
-              transition
-            "
-          >
+          <button className="w-full bg-yellow-500 hover:bg-yellow-400 text-black font-bold text-lg py-5 transition">
             ☰ CATEGORÍAS
           </button>
 
-          <div
-            className="
-              absolute
-              left-0
-              top-full
-              hidden
-              group-hover:block
-              bg-white
-              shadow-xl
-              w-full
-              z-50
-            "
-          >
+          <div className="absolute left-0 top-full hidden group-hover:block bg-white shadow-xl w-full z-50">
 
             <button
-  onClick={() => {
-    console.log("Herramientas");
-    setCategoriaSeleccionada?.("herramientas");
-  }}
-  className="block w-full text-left px-5 py-3 hover:bg-gray-100"
->
-  🔧 Herramientas
-</button>
+              onClick={() => setCategoriaSeleccionada?.("herramientas")}
+              className="block w-full text-left px-5 py-3 hover:bg-gray-100"
+            >
+              🔧 Herramientas
+            </button>
 
             <button
               onClick={() => setCategoriaSeleccionada?.("electricidad")}
@@ -96,54 +67,50 @@ export default function Navbar({
 
         </div>
 
-        {/* MENÚ PRINCIPAL */}
-
+        {/* MENÚ */}
         <div className="flex-1 flex justify-center">
 
           <div className="flex gap-20">
 
-  <Link
-    href="/"
-    className={`font-bold text-lg py-5 border-b-4 transition ${
-      pathname === "/"
-        ? "text-yellow-500 border-yellow-500"
-        : "text-white border-transparent hover:text-yellow-500"
-    }`}
-  >
-    INICIO
-  </Link>
+            <Link
+              href="/"
+              className={`font-bold text-lg py-5 border-b-4 transition ${
+                pathname === "/"
+                  ? "text-yellow-500 border-yellow-500"
+                  : "text-white border-transparent hover:text-yellow-500"
+              }`}
+            >
+              INICIO
+            </Link>
 
-  <Link
-    href="/productos"
-    className={`font-bold text-lg py-5 border-b-4 transition ${
-      pathname === "/productos"
-        ? "text-yellow-500 border-yellow-500"
-        : "text-white border-transparent hover:text-yellow-500"
-    }`}
-  >
-    PRODUCTOS
-  </Link>
+            <Link
+              href="/productos"
+              className={`font-bold text-lg py-5 border-b-4 transition ${
+                pathname === "/productos"
+                  ? "text-yellow-500 border-yellow-500"
+                  : "text-white border-transparent hover:text-yellow-500"
+              }`}
+            >
+              PRODUCTOS
+            </Link>
 
-  <Link
-    href="/ofertas"
-    className={`font-bold text-lg py-5 border-b-4 transition ${
-      pathname === "/ofertas"
-        ? "text-yellow-500 border-yellow-500"
-        : "text-white border-transparent hover:text-yellow-500"
-    }`}
-  >
-    OFERTAS
-  </Link>
+            <Link
+              href="/ofertas"
+              className={`font-bold text-lg py-5 border-b-4 transition ${
+                pathname === "/ofertas"
+                  ? "text-yellow-500 border-yellow-500"
+                  : "text-white border-transparent hover:text-yellow-500"
+              }`}
+            >
+              OFERTAS
+            </Link>
 
-  <Link
-    href="/#contacto"
-    className="text-white font-bold text-lg py-5 hover:text-yellow-500 transition"
-  >
-    CONTACTO
-  </Link>
-
-</div>
-      
+            <Link
+              href="/#contacto"
+              className="text-white font-bold text-lg py-5 hover:text-yellow-500 transition"
+            >
+              CONTACTO
+            </Link>
 
           </div>
 
