ab0eeae8c28859a41afd09edc90fc356692344ca
gc190311-svg <gc190311@gmail.com>
2026-06-25 20:21:34 -0500

    Update OrdenarProductos.jsx

--- components/catalogo/OrdenarProductos.jsx
+++ components/catalogo/OrdenarProductos.jsx
@@ -1,1 +1,51 @@
+"use client";
 
+export default function OrdenarProductos() {
+
+  return (
+
+    <div className="flex justify-between items-center mb-8">
+
+      <h2 className="text-2xl font-bold">
+
+        Todos los productos
+
+      </h2>
+
+      <select
+        className="
+          border
+          rounded-xl
+          px-5
+          py-3
+          bg-white
+        "
+      >
+
+        <option>
+          Ordenar por
+        </option>
+
+        <option>
+          Precio: Menor a mayor
+        </option>
+
+        <option>
+          Precio: Mayor a menor
+        </option>
+
+        <option>
+          Más recientes
+        </option>
+
+        <option>
+          Destacados
+        </option>
+
+      </select>
+
+    </div>
+
+  );
+
+}
