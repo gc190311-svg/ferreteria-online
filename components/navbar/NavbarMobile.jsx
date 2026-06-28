e61420b9ae3f9a1fb0519405fe916e5efe495e7c
gc190311-svg <gc190311@gmail.com>
2026-06-27 20:19:16 -0500

    Update NavbarMobile.jsx

--- components/navbar/NavbarMobile.jsx
+++ components/navbar/NavbarMobile.jsx
@@ -1,1 +1,23 @@
+"use client";
 
+export default function NavbarMobile() {
+
+  return (
+
+    <nav className="bg-black border-y border-gray-800">
+
+      <div className="h-16 flex items-center px-5">
+
+        <button className="text-white text-3xl">
+
+          ☰
+
+        </button>
+
+      </div>
+
+    </nav>
+
+  );
+
+}
