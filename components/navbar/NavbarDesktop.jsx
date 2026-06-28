8e4688c20cba7eedca2e2ad1820be23060ab765e
gc190311-svg <gc190311@gmail.com>
2026-06-27 20:18:57 -0500

    Update NavbarDesktop.jsx

--- components/navbar/NavbarDesktop.jsx
+++ components/navbar/NavbarDesktop.jsx
@@ -1,1 +1,23 @@
+"use client";
 
+export default function NavbarDesktop() {
+
+  return (
+
+    <nav className="bg-black border-y border-gray-800">
+
+      <div className="max-w-7xl mx-auto h-16 flex items-center justify-center">
+
+        <span className="text-white">
+
+          Navbar Desktop
+
+        </span>
+
+      </div>
+
+    </nav>
+
+  );
+
+}
