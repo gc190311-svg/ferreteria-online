0d06333fa96c7d5786e26728065a71df508e62d1
gc190311-svg <gc190311@gmail.com>
2026-06-29 11:28:14 -0500

    Update page.jsx

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -1,40 +1,43 @@
 "use client";
 
+import { useState } from "react";
+
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
-import BannerCatalogo from "../../components/catalogo/BannerCatalogo";
 import SidebarFiltros from "../../components/catalogo/SidebarFiltros";
 import BuscadorCatalogo from "../../components/catalogo/BuscadorCatalogo";
 import GridProductos from "../../components/catalogo/GridProductos";
-import OrdenarProductos from "../../components/catalogo/OrdenarProductos";
-
-import { CatalogoProvider } from "../../components/context/CatalogoContext";
 
 export default function CatalogoProductos() {
 
+  const [categoriaSeleccionada, setCategoriaSeleccionada] =
+    useState("todos");
+
   return (
-
-    <CatalogoProvider>
-
+    <>
       <Header />
 
-      <Navbar />
-
-      <BannerCatalogo />
+      <Navbar
+        categoriaSeleccionada={categoriaSeleccionada}
+        setCategoriaSeleccionada={setCategoriaSeleccionada}
+      />
 
       <div className="max-w-7xl mx-auto flex gap-8 py-10 px-4">
 
-        <SidebarFiltros />
+        <SidebarFiltros
+          categoriaSeleccionada={categoriaSeleccionada}
+          setCategoriaSeleccionada={setCategoriaSeleccionada}
+        />
 
         <div className="flex-1">
 
           <BuscadorCatalogo />
 
-          <OrdenarProductos />
-
-          <GridProductos />
+          <GridProductos
+            categoriaSeleccionada={categoriaSeleccionada}
+          />
 
         </div>
 
@@ -42,8 +45,7 @@ export default function CatalogoProductos() {
 
       <Footer />
 
-    </CatalogoProvider>
-
+    </>
   );
 
 }
