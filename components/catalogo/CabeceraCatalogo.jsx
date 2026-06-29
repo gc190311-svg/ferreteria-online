d0c23eda30bc74812e1033480f0864405beaefe6
gc190311-svg <gc190311@gmail.com>
2026-06-29 11:50:23 -0500

    Update CabeceraCatalogo.jsx

--- components/catalogo/CabeceraCatalogo.jsx
+++ components/catalogo/CabeceraCatalogo.jsx
@@ -1,6 +1,35 @@
 "use client";
 
 export default function CabeceraCatalogo() {
-  return null;
+  return (
+    <div className="flex items-center justify-between mb-8">
+
+      <div>
+
+        <h1 className="text-4xl font-bold text-gray-900">
+          Resultados encontrados
+        </h1>
+
+        <p className="text-gray-500 mt-2">
+          Mostrando todos los productos
+        </p>
+
+      </div>
+
+      <div
+        className="
+          bg-yellow-100
+          text-yellow-900
+          px-6
+          py-3
+          rounded-xl
+          font-semibold
+        "
+      >
+        Catálogo Brico Hogar
+      </div>
+
+    </div>
+  );
 }
 
