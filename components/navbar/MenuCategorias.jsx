bef37e5eb194c733589607ecfd363093e4def09c
gc190311-svg <gc190311@gmail.com>
2026-06-29 21:44:17 -0500

    Update MenuCategorias.jsx

--- components/navbar/MenuCategorias.jsx
+++ components/navbar/MenuCategorias.jsx
@@ -1,155 +1,34 @@
 "use client";
 
 import Link from "next/link";
-import {
-  FaTools,
-  FaBolt,
-  FaPaintRoller,
-  FaFaucet,
-  FaHardHat,
-  FaChevronRight,
-} from "react-icons/fa";
-
-const categorias = [
-  {
-    nombre: "Herramientas",
-    icono: FaTools,
-    url: "/productos?categoria=herramientas",
-  },
-  {
-    nombre: "Electricidad",
-    icono: FaBolt,
-    url: "/productos?categoria=electricidad",
-  },
-  {
-    nombre: "Construcción",
-    icono: FaHardHat,
-    url: "/productos?categoria=construccion",
-  },
-  {
-    nombre: "Pintura",
-    icono: FaPaintRoller,
-    url: "/productos?categoria=pintura",
-  },
-  {
-    nombre: "Gasfitería",
-    icono: FaFaucet,
-    url: "/productos?categoria=gasfiteria",
-  },
-];
 
 export default function MenuCategorias() {
+
   return (
 
-    <div className="relative group w-[260px]">
+    <div className="w-[80px]">
 
-      <button
-        className="
-          w-full
-          bg-yellow-500
-          hover:bg-yellow-400
-          text-black
-          font-bold
-          py-5
-          transition
-        "
-      >
-        ☰ CATEGORÍAS
-      </button>
+      <Link href="/categorias">
 
-      <div
-        className="
-          absolute
-          top-full
-          left-0
-          hidden
-          group-hover:block
-          w-80
-          bg-white
-          rounded-b-xl
-          shadow-2xl
-          overflow-hidden
-          z-50
-        "
-      >
-
-        {categorias.map((categoria) => {
-
-          const Icono = categoria.icono;
-
-          return (
-
-            <Link
-              key={categoria.nombre}
-              href={categoria.url}
-            >
-
-              <div
-                className="
-                  flex
-                  items-center
-                  justify-between
-                  px-6
-                  py-4
-                  hover:bg-yellow-50
-                  transition
-                  cursor-pointer
-                "
-              >
-
-                <div className="flex items-center gap-4">
-
-                  <Icono
-                    className="
-                      text-yellow-500
-                      text-xl
-                    "
-                  />
-
-                  <span className="font-medium">
-
-                    {categoria.nombre}
-
-                  </span>
-
-                </div>
-
-                <FaChevronRight
-                  className="
-                    text-gray-400
-                  "
-                />
-
-              </div>
-
-            </Link>
-
-          );
-
-        })}
-
-        <Link
-          href="/productos"
+        <button
+          className="
+            w-full
+            bg-yellow-500
+            hover:bg-yellow-400
+            text-black
+            text-3xl
+            font-bold
+            py-5
+            transition
+            flex
+            items-center
+            justify-center
+          "
         >
+          ☰
+        </button>
 
-          <div
-            className="
-              bg-yellow-500
-              hover:bg-yellow-400
-              text-center
-              py-4
-              font-bold
-              transition
-            "
-          >
-
-            Ver todo el catálogo →
-
-          </div>
-
-        </Link>
-
-      </div>
+      </Link>
 
     </div>
 
