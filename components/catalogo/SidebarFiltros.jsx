2da4d21c0167f0fd1703f84572b04141524e00d2
gc190311-svg <gc190311@gmail.com>
2026-07-02 17:25:34 -0500

    Update SidebarFiltros.jsx

--- components/catalogo/SidebarFiltros.jsx
+++ components/catalogo/SidebarFiltros.jsx
@@ -50,14 +50,15 @@ export default function SidebarFiltros() {
   "
 >
 
-      <h2
-        className="
-          text-3xl
-          font-bold
-          text-gray-900
-          mb-8
-        "
-      >
+     <h2
+  className="
+    text-4xl
+    font-bold
+    mb-8
+  "
+>
+  Marcas
+</h2>
         Marcas
       </h2>
 
