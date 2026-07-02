1c4083403c484d8a81475c78452a8c31f0f454a8
gc190311-svg <gc190311@gmail.com>
2026-07-02 15:18:02 -0500

    Update GridProductos.jsx

--- components/catalogo/GridProductos.jsx
+++ components/catalogo/GridProductos.jsx
@@ -66,15 +66,16 @@ export default function GridProductos() {
 
   return (
 
-    <div
-      className="
-        grid
-        grid-cols-2
-        md:grid-cols-3
-        xl:grid-cols-4
-        gap-8
-      "
-    >
+  <div
+  className="
+    grid
+    grid-cols-2
+    md:grid-cols-2
+    lg:grid-cols-3
+    gap-8
+    items-start
+  "
+>
 
       {productosFiltrados.map((producto) => (
 
