53daa4a89dc29abb185aef0c3e531febf5ca7415
gc190311-svg <gc190311@gmail.com>
2026-07-08 17:49:47 -0500

    Update CarritoContext.jsx

--- components/carrito/CarritoContext.jsx
+++ components/carrito/CarritoContext.jsx
@@ -1,1 +1,29 @@
+"use client";
 
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+const CarritoContext = createContext();
+
+export function CarritoProvider({ children }) {
+
+  // Estados
+
+  // Funciones
+
+  // Totales
+
+  // LocalStorage
+
+  // Provider
+
+}
+
+export function useCarrito() {
+  return useContext(CarritoContext);
+}
