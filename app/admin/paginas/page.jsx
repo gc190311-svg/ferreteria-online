7e1481a475997e6fe7b12b16b06ecf9218e34b09
Giancarlo Alexis Valcarcel Quinto <gc190311@gmail.com>
2026-08-03 19:46:23 -0500

    Versión lista para producción

---
+++ app/admin/paginas/page.jsx
@@ -0,0 +1,13 @@
+export default function PaginasAdmin() {
+    return (
+        <div className="p-10">
+            <h1 className="text-3xl font-bold">
+                Administración de páginas
+            </h1>
+
+            <p className="mt-4">
+                Próximamente...
+            </p>
+        </div>
+    );
+}
